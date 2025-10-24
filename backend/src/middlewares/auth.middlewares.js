0028a10f7abe917275a543390f1a2f8272c90fb4
Arbaz Khan <arbazkha78az@gmail.com>
2025-10-24 20:28:58 +0530

    auth middleware

---
+++ backend/src/middlewares/auth.middlewares.js
@@ -0,0 +1,36 @@
+const FoodPartnerModel = require("../models/foodPartner.model");
+const jwt = require("jsonwebtoken");
+
+async function authFoodPartnerMiddleware (req, res, next) {
+    const token = req.cookies.token;
+    console.log("cookies", req.cookies)
+
+    //checking user having token or not
+    if(!token) {
+        return res.status(401).json({
+            message: "Please login first"
+        })
+    }
+
+    try {
+        //checking token is valid
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        //Food-Partner
+        const foodPartner = await FoodPartnerModel.findById(decoded.id);
+
+        req.foodPartner = foodPartner;
+
+        next();
+    }
+    catch (err) {
+        return res.status(401).json({
+            message: "Invalid token"
+        })
+    }
+}
+
+module.exports = {
+    authFoodPartnerMiddleware,
+
+}
