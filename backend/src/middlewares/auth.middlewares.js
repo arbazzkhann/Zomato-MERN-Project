6db87df2451f44a16d4e78fb65cf7a9dce295045
Arbaz Khan <arbazkha78az@gmail.com>
2025-10-29 20:49:16 +0530

    authUserMiddleware

--- backend/src/middlewares/auth.middlewares.js
+++ backend/src/middlewares/auth.middlewares.js
@@ -1,7 +1,8 @@
 const FoodPartnerModel = require("../models/foodPartner.model");
 const jwt = require("jsonwebtoken");
+const UserModel = require("../models/user.model");
 
-async function authFoodPartnerMiddleware (req, res, next) {
+async function authFoodPartnerMiddleware(req, res, next) {
     const token = req.cookies.token;
     console.log("cookies", req.cookies)
 
@@ -30,6 +31,34 @@ async function authFoodPartnerMiddleware (req, res, next) {
     }
 }
 
+async function authUserMiddleware(req, res, next) {
+    const token = req.cookies.token;
+
+    if(!token) {
+        return res.status(200).json({
+            message: "Please login first"
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await UserModel.findById(decoded._id);
+
+        req.user = user;
+
+        next();
+    }
+    catch(err) {
+        res.status(401).json({
+            message: "Invalid token",
+            err
+        });
+    }
+}
+
 module.exports = {
     authFoodPartnerMiddleware,
+    authUserMiddleware,
+    
 }
