df3cedf3606147fcd9b7a4c3517e33cf82e5e19d
Arbaz Khan <arbazkha78az@gmail.com>
2025-10-31 18:25:50 +0530

    adding phoneNumber in userModel

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -5,6 +5,10 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true
     },
+    phoneNumber: {
+        type: Number,
+        required: true
+    },
     email: {
         type: String,
         required: true,
