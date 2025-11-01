6fa57f4801a8bec94f392fa43213c3051e28e43f
Arbaz Khan <arbazkha78az@gmail.com>
2025-11-01 13:54:12 +0530

    connecting userRegister component with backend

--- frontend/src/components/auth/UserRegister.jsx
+++ frontend/src/components/auth/UserRegister.jsx
@@ -1,19 +1,57 @@
 import { Link } from 'react-router-dom';
 import './Auth.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const UserRegister = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const fullName = e.target.fullName.value;
+    const email = e.target.email.value;
+    const phoneNumber = e.target.phoneNumber.value;
+    const password = e.target.password.value;
+
+    const response = await axios.post("http://localhost:3000/api/auth/user/register", {
+      fullName,
+      email,
+      phoneNumber,
+      password
+    }, {
+      withCredentials: true  //saving cookies
+    });
+
+    console.log(response.data);
+
+    navigate("/");
+  }
+
+  
+
   return (
     <div className="auth-container">
       <div className="auth-form">
         <h2 className="auth-title">User Registration</h2>
-        <form>
+        <form method='post' onSubmit={handleSubmit}>
           <div className="form-group">
-            <label htmlFor="name" className="form-label">Full Name</label>
+            <label htmlFor="fullName" className="form-label">Full Name</label>
             <input
               type="text"
-              id="name"
+              id="fullName"
               className="form-input"
               placeholder="Enter your full name"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="phoneNumber" className="form-label">Phone Number</label>
+            <input
+              type="number"
+              id="phoneNumber"
+              className="form-input"
+              placeholder="Enter your phone number"
               required
             />
           </div>
@@ -37,7 +75,7 @@ const UserRegister = () => {
               required
             />
           </div>
-          <button type="submit" className="auth-button">Register</button>
+          <button type='submit' className="auth-button">Register</button>
         </form>
         <div className="auth-switch">
           Already have an account? <Link to="/user/login">Login here</Link>
