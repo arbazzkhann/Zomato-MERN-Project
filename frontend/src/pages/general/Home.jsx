4d89cba8790d9cce961af37012f024ff71e9e933
Arbaz Khan <arbazkha78az@gmail.com>
2025-11-24 22:32:08 +0530

    adding like video function

--- frontend/src/pages/general/Home.jsx
+++ frontend/src/pages/general/Home.jsx
@@ -64,6 +64,25 @@ const Home = () => {
   const goPrev = () => scrollToIndex(getIndex() - 1);
   const goNext = () => scrollToIndex(getIndex() + 1);
 
+  //like and dislike video
+  async function likeVideo(item) {
+    const response = await axios.post(
+      "http://localhost:3000/api/food/like", 
+      {foodId: item._id}, 
+      {withCredentials: true}
+    );
+
+    console.log("response: ", response)
+
+    if(response.data.like) {
+      console.log("Video liked");
+      setVideos((prev) => prev.map((v) => v._id === item._id ? { ...v, likesCount: v.likeCount + 1 } : v));
+    }
+    else {
+      console.log("Video disliked");
+      setVideos((prev) => prev.map((v) => v._id === item._id ? { ...v, likesCount: v.likeCount - 1 } : v));
+    }
+  }
 
   console.log(videos)
   // ---------------------------------------
@@ -111,7 +130,7 @@ const Home = () => {
             <div className="action-stack">
               {/* LIKE BUTTON */}
               <div>
-                <button className="action-btn">
+                <button onClick={() => likeVideo(item)} className="action-btn">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icon">
                     <path fill="none" stroke="white" strokeWidth="1.8" 
                       d="M12.001 4.52853C14.35 2.42 17.98 2.49 20.2426 4.75736C22.5053 7.02472 22.583 10.637 20.4786 12.993L11.9999 21.485L3.52138 12.993C1.41705 10.637 1.49571 7.01901 3.75736 4.75736C6.02157 2.49315 9.64519 2.41687 12.001 4.52853Z"/>
